72fcb881ced2425666c817452efa662d1e5a83fb
Olaf Klischat <olaf.klischat@gmail.com>
2020-07-14 09:19:14 +0200

    travis fixed

--- hack/ci/ci-oklischat-build-and-push-all.sh
+++ hack/ci/ci-oklischat-build-and-push-all.sh
@@ -3,10 +3,10 @@
 set -e
 
 repo="$1"
-tag="$2"
+shift
 
-if [[ -z "$tag" ]]; then
-  echo "no tag specified; not building and pushing an image."
+if [[ -z "$*" ]]; then
+  echo "no tags specified; not building and pushing an image."
   exit 0
 fi
 
@@ -16,5 +16,5 @@ cd "$(dirname $0)/../.."
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v "`pwd`:/go/src/github.com/kubermatic/kubermatic" --entrypoint=/bin/bash quay.io/kubermatic/go-docker:14.2-1806-0 -c "
   set -e
   docker login -u '$DOCKER_USERNAME' -p '$DOCKER_PASSWORD';
-  DOCKER_REPO=oklischat KUBERMATIC_EDITION=ee /go/src/github.com/kubermatic/kubermatic/hack/push_image.sh $tag;
+  DOCKER_REPO=$repo KUBERMATIC_EDITION=ee /go/src/github.com/kubermatic/kubermatic/hack/push_image.sh $*;
 "
